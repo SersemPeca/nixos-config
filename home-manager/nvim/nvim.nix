e9f4fd314401e758eaaa2347e9d8b0a1de45603b
Petar Atanasov <p.atanasov21@abv.bg>
2025-08-04 17:02:05 +0300

    feat: install all grammars

--- home-manager/nvim/nvim.nix
+++ home-manager/nvim/nvim.nix
@@ -43,6 +43,9 @@
       oil.enable = true;
       treesitter = {
         enable = true;
+
+        grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
+
         settings = {
           indent.enable = true;
           highlight.enable = true;
