5bacd8e5ed083c93ff20ea612e1cf833bf7a9bdd
Petar Atanasov <p.atanasov21@abv.bg>
2025-10-31 20:20:40 +0200

    feat: Add ts language server

--- home-manager/nvim/nvim.nix
+++ home-manager/nvim/nvim.nix
@@ -54,8 +54,6 @@
       #   enable = true;
       # };
 
-      # Agentic coding
-
       # vimtex = {
       #   enable = true;
       # avante = {
@@ -65,6 +63,42 @@
       #     auto_suggestions_provider = "copilot";
       #   };
       # };
+
+      image = {
+        enable = true;
+      };
+
+      molten = {
+        enable = true;
+
+        # Configuration settings for molten.nvim. More examples at https://github.com/nix-community/nixvim/blob/main/plugins/by-name/molten/default.nix#L191
+        settings = {
+          auto_image_popup = false;
+          auto_init_behavior = "init";
+          auto_open_html_in_browser = false;
+          auto_open_output = true;
+          cover_empty_lines = false;
+          copy_output = false;
+          enter_output_behavior = "open_then_enter";
+          image_provider = "none";
+          output_crop_border = true;
+          output_virt_lines = false;
+          output_win_border = [
+            ""
+            "━"
+            ""
+            ""
+          ];
+          output_win_hide_on_leave = true;
+          output_win_max_height = 15;
+          output_win_max_width = 80;
+          save_path.__raw = "vim.fn.stdpath('data')..'/molten'";
+          tick_rate = 500;
+          use_border_highlights = false;
+          limit_output_chars = 10000;
+          wrap_output = false;
+        };
+      };
     };
 
     extraPackages = with pkgs; [
@@ -124,6 +158,7 @@
         clangd.enable = true;
         nimls.enable = true;
         lua_ls.enable = true;
+        ts_ls.enable = true;
       };
     };
 
